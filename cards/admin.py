0555906c6c3b2ecdb38a3619b5b6679ab94541aa
Павел Павлов <pavelpavlov@MacBook-Air-Pavel.local>
2022-04-16 01:41:43 +0500

    Add models

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, TrackParameter
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TrackParameter)
+class TrackParameterAdmin(admin.ModelAdmin):
+    pass
--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+
+TIME_INTERVAL_CHOICES = [
+    ('1 hour', '1 час'),
+    ('12 hours', '12 часов'),
+    ('24 hours', '24 часа'),
+]
+
+
+class Product(models.Model):
+    """Contains information about the product"""
+    article = models.CharField(
+        verbose_name='артикул',
+        max_length=10,
+        help_text='Артикул товара',
+    )
+    name = models.CharField(
+        verbose_name='наименование товара',
+        max_length=100,
+    )
+    price_without_discount = models.IntegerField(
+        verbose_name='Цена без скидки',
+        validators=[MinValueValidator(0)],
+        help_text='Цена без скидки в копейках',
+    )
+    price_with_discount = models.IntegerField(
+        verbose_name='Цена со скидкой',
+        validators=[MinValueValidator(0)],
+        help_text='Цена со скидкой в копейках',
+    )
+    brand = models.CharField(
+        verbose_name='бренд товара',
+        max_length=50,
+    )
+    seller = models.CharField(
+        verbose_name='поставщик',
+        max_length=100,
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return f'{self.brand}/{self.name}'
+
+
+class TrackParameter(models.Model):
+
+    article = models.CharField(
+        verbose_name='артикул',
+        max_length=10,
+        help_text='Артикул товара',
+    )
+    start_at = models.DateTimeField(
+        verbose_name='начало отслеживания',
+        db_index=True,
+        help_text='Дата начала периода отслеживания товара',
+    )
+    end_at = models.DateTimeField(
+        verbose_name='Конец отслеживания',
+        db_index=True,
+        help_text='Дата конца периода отслеживания товара',
+    )
+    time_interval = models.CharField(
+        'Длительность аренды',
+        max_length=10,
+        choices=TIME_INTERVAL_CHOICES,
+        default='1hour')
+    user = models.ManyToManyField(
+        User,
+        related_name='parameters',
+        verbose_name='Пользователь')
+
+    class Meta:
+        verbose_name = 'Параметр отслеживания'
+        verbose_name_plural = 'Параметры отслеживания'
+
+    def __str__(self):
+        return f'{self.article}:{self.start_at}:{self.end_at}'
+
