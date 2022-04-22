602805cc1d9861f01018d8fd88dbc186776d056e
Fiskless <fiskless@gmail.com>
2022-04-23 03:13:01 +0500

    correct urls

--- api/urls.py
+++ api/urls.py
@@ -6,10 +6,10 @@ from api.views import TrackParameterCreateView, TrackParameterListView, \
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('user/track_create/', TrackParameterCreateView.as_view()),
-    path('user/tracks/', TrackParameterListView.as_view()),
-    path('user/track/<int:pk>/', TrackParameterDetailView.as_view()),
-    path('user/track/<int:pk>/products/', ProductsForTrackParameterListView.as_view()),
-    path('user/products/', ProductsListView.as_view()),
-    path('user/product/<int:article>/', ProductListView.as_view())
+    path('user/track_create/', TrackParameterCreateView.as_view(), name='create_new_track'),
+    path('user/tracks/', TrackParameterListView.as_view(), name='get_all_tracks'),
+    path('user/track/<int:pk>/', TrackParameterDetailView.as_view(), name='get_track'),
+    path('user/track/<int:pk>/products/', ProductsForTrackParameterListView.as_view(), name='get_track_products'),
+    path('user/products/', ProductsListView.as_view(), name='get_user_products'),
+    path('user/product/<int:article>/', ProductListView.as_view(), name='get_product_info')
 ]
