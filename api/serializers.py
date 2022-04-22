7d14e4e529ccb8fa975da70526d7a96e7b6918ef
Fiskless <fiskless@gmail.com>
2022-04-23 03:12:36 +0500

    add field to serializer

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,8 @@ from django.utils.timezone import utc
 from rest_framework import serializers
 from cards.models import TrackParameter, Product
 from cards.tasks import update_product_data
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class TrackParameterSerializer(serializers.ModelSerializer):
@@ -14,16 +16,18 @@ class TrackParameterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         track_parameter = self.validated_data
-        if track_parameter['end_at'] < datetime.datetime.utcnow().replace(tzinfo=utc):
-            raise serializers.ValidationError({'error': "End time should be more current time"})
-        if track_parameter['start_at'] >= track_parameter['end_at']:
-            raise serializers.ValidationError({'error': 'Start time should be less end time'})
-        if track_parameter['start_at'] < datetime.datetime.utcnow().replace(tzinfo=utc):
-            raise serializers.ValidationError({'error': 'Start time should be more or equal current time'})
-        new_track = TrackParameter.objects.create(**validated_data)
-        new_track.user.add(self.context['request'].user)
-        res = update_product_data.delay(track_parameter['article'])
-        return new_track
+        if track_parameter:
+            if track_parameter['end_at'] < datetime.datetime.utcnow().replace(tzinfo=utc):
+                raise serializers.ValidationError({'error': "End time should be more current time"})
+            if track_parameter['start_at'] >= track_parameter['end_at']:
+                raise serializers.ValidationError({'error': 'Start time should be less end time'})
+            if track_parameter['start_at'] < datetime.datetime.utcnow().replace(tzinfo=utc):
+                raise serializers.ValidationError({'error': 'Start time should be more or equal current time'})
+            new_track = TrackParameter.objects.create(**validated_data)
+            new_track.user.add(self.context['request'].user)
+            res = update_product_data.delay(track_parameter['article'])
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class TrackParameterListSerializer(serializers.ModelSerializer):
@@ -37,4 +41,4 @@ class ProductListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['article', 'name', 'price_without_discount', 'price_with_discount', 'brand', 'seller']
+        fields = ['article', 'name', 'price_without_discount', 'price_with_discount', 'brand', 'seller', 'time']
